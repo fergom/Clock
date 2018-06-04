386b3d10978e35b5c17bed28f9f9e88e10b01700
Fernando Gómez <al286313@uji.es>
2018-06-04 16:55:28 +0200

    Clock desktop.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  counter = 0;
+  private ms = 0;
+
+  MONTHS_CONST = 12;
+  HOURS_CONST = 24;
+  MINUTES_SECONDS_CONST = 60;
+
+  years = 0;
+  months = 0;
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
 
   ngOnInit(): void {
+    const then = '10/12/2017';
+    this.ms = moment.duration(moment().diff(moment(then, 'DD/MM/YYYY'))).asMilliseconds();
+    this.updateVariables();
+
     setInterval(() => {
-      this.counter++;
+      this.ms += 1000;
+      this.updateVariables();
     }, 1000);
+  }
+
+  getDaysOfMonth(): number {
+    return moment().daysInMonth();
+  }
+
+  private updateVariables(): void {
+    const duration = moment.duration(this.ms, 'milliseconds');
+
+    this.years = Math.floor(duration.asYears());
+    this.months = Math.floor(duration.asMonths());
+    this.days = Math.floor(duration.asDays());
+    this.hours = Math.floor(duration.asHours());
+    this.minutes = Math.floor(duration.asMinutes());
+    this.seconds = Math.floor(duration.asSeconds());
   }
 
 }
